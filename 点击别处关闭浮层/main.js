329e33cf95bcc18bb4cdb7511a09d97c90eca38d
xianjiezh <904292766@qq.com>
2018-01-16 20:51:08 +0800

    添加了contains，解决了浮层子代节点过多时的bug

--- 点击别处关闭浮层/main.js
+++ 点击别处关闭浮层/main.js
@@ -2,8 +2,7 @@ let one = document.getElementsByClassName('one')[0]
 let two = document.getElementsByClassName('two')[0]
 let check = document.getElementById('check')
 one.addEventListener('click',function(e){
-    
-    if(e.target !== check && e.target !== two){
+    if((e.target !== two) && (!two.contains(e.target))){
         if(two.style.display == 'block'){
             two.style.display = 'none'
         } else{
@@ -11,12 +10,9 @@ one.addEventListener('click',function(e){
             // 在浮层出现时监听，节约资源
             document.addEventListener('click',function(e){
                 if(e.target === document.documentElement){
-                    console.log('document')
                     two.style.display = 'none'
                 }
             })
         }
     }
-    
-    
 })
