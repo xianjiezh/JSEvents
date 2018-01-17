5aa091876bfdf0f429e288833aedc03044bb6e37
xianjiezh <904292766@qq.com>
2018-01-17 23:53:36 +0800

    实现开始只监听一次

--- 点击别处关闭浮层/main.js
+++ 点击别处关闭浮层/main.js
@@ -1,17 +1,23 @@
 let one = document.getElementsByClassName('one')[0]
 let two = document.getElementsByClassName('two')[0]
 let check = document.getElementById('check')
-one.addEventListener('click',function(e){
-    if((e.target !== two) && (!two.contains(e.target))){
-        if(two.style.display == 'block'){
+one.addEventListener('click', function (e) {
+    if ((e.target !== two) && (!two.contains(e.target))) {
+        if (two.style.display == 'block') {
             two.style.display = 'none'
-        } else{
+        } else {
             two.style.display = 'block'
             // 在浮层出现时监听，节约资源
-            document.addEventListener('click',function(e){
-                if(e.target === document.documentElement){
-                    two.style.display = 'none'
+            document.addEventListener('click', function (e) {
+                console.log('document')
+                event.target.removeEventListener(event.type, arguments.callee);
+                function callback(e) {
+                    if (e.target === document.documentElement) {
+                        two.style.display = 'none'
+                    }
                 }
+                return callback(e)
+
             })
         }
     }
